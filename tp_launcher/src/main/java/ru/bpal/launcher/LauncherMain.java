62fdc4b50bfb43fa657a78cea95aae8059723434
Sergey Danilov <sergeydanilov@gmail.com>
2015-12-02 09:00:45 +0800

    added launcher project to demo file io and others thins

---
+++ tp_launcher/src/main/java/ru/bpal/launcher/LauncherMain.java
@@ -0,0 +1,14 @@
+package ru.bpal.launcher;
+
+
+/**
+ * Created by serg on 16.04.15.
+ */
+public class LauncherMain {
+
+
+
+    public static void main(String [] args) {
+        System.out.println("Hello World!");
+    }
+}
